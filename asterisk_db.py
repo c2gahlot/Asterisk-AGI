8895309b48d5e72b743f9938660366e60f55e964
c2gahlot <c2gahlot@gmail.com>
2019-09-17 12:16:58 +0530

    Improvements added as per new schema

--- asterisk_db.py
+++ asterisk_db.py
@@ -3,23 +3,21 @@ import mysql.connector
 
 def format_query_result(cursor, type='many'):
     columns = [col[0] for col in cursor.description]
-    if type=='many':
+    if type == 'many':
         rows = [dict(zip(columns, row)) for row in cursor.fetchall()]
-    else:
+    elif type == 'one':
         rows = dict(zip(columns, cursor.fetchone()))
+    else:
+        rows = []
     return rows
 
 
 def get_call_details(caller_id):
-
-    # create the connection object
     myconn = mysql.connector.connect(host="localhost", user="root", passwd="root", database="my_operator")
-
-    # creating the cursor object
     cur = myconn.cursor(buffered=True)
-
-    # creating query string
-    sql = '''select callers.*, companies.name as company_name from callers left join companies on callers.company_id = companies.id where dnid = '{dnid}' '''.format(dnid=caller_id)
+    sql = ''' select callers.*, companies.name as company_name from callers 
+        left join companies on callers.company_id = companies.id 
+        where dnid = '{}' '''.format(caller_id)
 
     try:
         # inserting the values into the table
@@ -32,5 +30,23 @@ def get_call_details(caller_id):
 
     details = format_query_result(cur, 'one')
     myconn.close()
+    return details
 
+
+def get_extension_plan(company_id, context, time_now):
+    myconn = mysql.connector.connect(host="localhost", user="root", passwd="root", database="my_operator")
+    cur = myconn.cursor(buffered=True)
+    sql = ''' select * from ivrs where company_id = {} and context = '{}' 
+        and ((start_time <= '{}' and end_time > '{}') 
+        or (start_time = '00:00:00' and end_time = '00:00:00')) '''.format(company_id, context, time_now, time_now)
+
+    try:
+        cur.execute(sql)
+        myconn.commit()
+    except Exception as exception:
+        print(exception)
+        myconn.rollback()
+
+    details = format_query_result(cur, 'one')
+    myconn.close()
     return details
--- ivr_calls.py
+++ ivr_calls.py
@@ -1,6 +1,7 @@
 #!/home/vagrant/code/Asterisk-AGI/.vagenv/bin/python
 
 import sys
+import datetime
 import asterisk_db
 from asterisk.agi import AGI
 
@@ -13,10 +14,13 @@ extension = sys.argv[4]
 timestamp = sys.argv[5]
 
 call_details = asterisk_db.get_call_details(caller_id)
-print(call_details)
+
+time_now = datetime.datetime.now().time()
+
+extension_plan = asterisk_db.get_extension_plan(call_details['company_id'], context, time_now)
+
 dial_str = 'SIP/shubham'
 agi.answer()
 result = agi.execute('EXEC DIAL {dial_str}'.format(dial_str=dial_str))
 agi.verbose(result, level=4)
-
-
+agi.hangup()
