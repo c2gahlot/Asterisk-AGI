f6a9598dbfdd6b46883b30c9d53e15123e1dc4d9
c2gahlot <c2gahlot@gmail.com>
2019-10-04 09:58:51 +0530

    Added AGI script to trigger a call

---
+++ make_call.py
@@ -0,0 +1,15 @@
+#!/home/vagrant/code/Asterisk-AGI/.vagenv/bin/python
+
+import sys
+from asterisk.agi import AGI
+
+agi = AGI()
+
+callee = sys.argv[1]
+
+
+def trigger_call(callee):
+    agi.execute('EXEC DIAL {},20,g'.format(callee))
+    agi.hangup()
+
+trigger_call(callee)
